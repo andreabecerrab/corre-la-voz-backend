f9f034c5692a231ae94ef43d7dce26af879c9322
morenoloza98 <morenolozalex98@gmail.com>
2020-06-04 13:16:06 -0500

    Added an example of jwt for near future reference

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+// Auth0 config
+
+const authConfig = {
+  domain: 'dev-hmnnmmhr.auth0.com',
+  audience: 'http://localhost:8081'
+}
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from dev-hmnnmmhr.auth0.com
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
+});
 
 // Initializationsrr
 const app = express();
@@ -36,6 +60,13 @@ router.use(function (req, res, next) {
 
 const indexRoute = require("./routes/index");
 app.use("/api", indexRoute);
+
+// Define an endpoint that must be called with an access token
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your Access Token was successfully validated!"
+  });
+});
 
 app.disable("etag");
 
