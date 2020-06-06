b27472af91e232675802efa8e0bbd22cef4df91c
morenoloza98 <morenolozalex98@gmail.com>
2020-06-06 17:59:15 -0500

    User in database successfully added

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,35 @@
 const Usuario = require("../models/Usuarios");
 
 exports.addUsuario = async function(req,res,next){
-    let userTemp = req.body.user_for_db;
-    console.log(userTemp);   
+    let user_name = req.body.name || '-';
+    let user_last_name = req.body.last_name || '-';
+    let user_email = req.body.email;
+    let user_type = req.body.type;
+    let user_sub = req.body.sub;
+    // console.log("user sub: " + user.sub);
+    let user_exists = await Usuario.findOne({ sub: user_sub }).then((data) => {
+        console.log(data);
+        
+            if(data === null){
+                let user = new Usuario({
+                    nombre: user_name,
+                    apellido: user_last_name,
+                    correo: user_email,
+                    sub: user_sub,
+                    tipo: user_type
+                });
+                try{
+                    user.save().then((data) => {
+                        res.json({ msg: user.name + " added."})
+                    })
+                } catch(err){
+                    res.status(500).json({ msg: err });
+                }
+            }
+        });
+}
+
+exports.getUsers = async function(req, res, next) {
+    let users = await Usuario.find({ sub: "google-oauth2|114213436716257432224" });
+    res.json(users);
 }
--- routes/signup.js
+++ routes/signup.js
@@ -1,12 +1,12 @@
 const express = require("express");
 var router = express.Router();
+const user_controller = require('../controllers/user-controller');
 
 const app = express();
 const checkJwt = require("../controllers/auth-controller");
 
 router.post("/usuario/inicio", checkJwt, (req, res, next) => {
-  console.log("pls wrk");
-  //user_controller.addUsuario(req, res, next);
+  user_controller.addUsuario(req, res, next);
 });
 
 module.exports = router;
