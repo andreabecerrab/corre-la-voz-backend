5486a88457418a789a9074207a2ef3ec2a178824
andreabecerrab <andii1wiki@gmail.com>
2020-06-06 03:29:43 -0500

    auth

--- routes/signup.js
+++ routes/signup.js
@@ -1,17 +1,12 @@
-// router.route("/usuarios").post(function (request, res) {
-//   const usuario = new Usuario();
+const express = require("express");
+var router = express.Router();
 
-//   usuario.nombre = request.body.nombre;
-//   usuario.apellido = request.body.apellido;
-//   usuario.correo = request.body.correo;
-//   usuario.contrasena = request.body.contrasena;
-//   usuario.tipo = request.body.tipo;
+const app = express();
+const checkJwt = require("../controllers/auth-controller");
 
-//   usuario.save(function (errores) {
-//     if (errores) {
-//       console.log(errores);
-//       res.send(errores);
-//     }
-//     res.json({ mensaje: "usiario creado" });
-//   });
-// });
+router.post("/usuario/inicio", checkJwt, (req, res, next) => {
+  console.log("pls wrk");
+  //user_controller.addUsuario(req, res, next);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,39 +3,12 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-<<<<<<< HEAD
-=======
-const user_controller = require('./controllers/user-controller');
->>>>>>> dbc6996de6d4ca444b7978fff23a32f2d5c644ec
-
-// Auth0 config
-
-const authConfig = {
-  domain: 'dev-hmnnmmhr.auth0.com',
-  audience: 'http://localhost:8081'
-}
-
-// Define middleware that validates incoming bearer tokens
-// using JWKS from dev-hmnnmmhr.auth0.com
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
-  }),
-
-  audience: authConfig.audience,
-  issuer: `https://${authConfig.domain}/`,
-  algorithm: ["RS256"]
-});
 
 // Initializationsrr
 const app = express();
 require("./database/database");
 app.use(require("./routes/index"));
+const checkJwt = require("./controllers/auth-controller");
 
 //conf
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -58,29 +31,26 @@ app.use(cors(corsOptions));
 router.use(function (req, res, next) {
   console.log("Getting in API");
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
   next();
 });
 
 const indexRoute = require("./routes/index");
+const userRoute = require("./routes/signup");
 app.use("/api", indexRoute);
+app.use("/auth", userRoute);
 
 // Define an endpoint that must be called with an access token
 app.get("/api/external", checkJwt, (req, res) => {
   res.send({
-    msg: "Your Access Token was successfully validated!"
+    msg: "Your Access Token was successfully validated!",
   });
 });
 
-<<<<<<< HEAD
-=======
-router.post('/usuario/inicio', checkJwt, (req, res, next) => {
-  console.log('pls wrk');
-  user_controller.addUsuario(req,res,next);
-});
-
->>>>>>> dbc6996de6d4ca444b7978fff23a32f2d5c644ec
 app.disable("etag");
 
 app.listen(PORT, () => {
